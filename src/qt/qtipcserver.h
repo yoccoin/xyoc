1facf4be0d644f5c01879978e56aea08cc7d2c77
pascalcoinclassic <pascalcoinclassic@gmx.com>
2017-10-06 10:09:07 +0530

    Initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define yocoinclassic-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "yocoinclassicURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
